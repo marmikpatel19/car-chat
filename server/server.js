521e88ec332029a1bf419a64190e0255731076ba
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-06-16 13:46:51 -0600

    refactor server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
-import express from "express";
-import cors from "cors";
-import posts from "./api/posts.route.js";
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./index");
+const port = process.env.PORT || 8000;
+
+connectDB();
 
 const app = express();
 
@@ -8,7 +11,12 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api/v1/posts", posts);
+app.use("/api/posts", require("./api/posts.route.js"));
 app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
 
-export default app;
+// Set port
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+module.exports = app;
