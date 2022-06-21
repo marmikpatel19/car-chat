de0754c9215f82b55961c35a47ce26c2e4343c6d
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-06-21 11:13:16 -0600

    add middleware

--- server/server.js
+++ server/server.js
@@ -29,6 +29,16 @@ async function connectDB() {
 
 connectDB();
 
+/* Middleware */
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+
+app.use(allowCrossDomain);
+
 /* Routes */
 app.use("/api/posts/admin", admin);
 
