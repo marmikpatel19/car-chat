0d84eb3ca890342abb5e1fb7a1f2ed83e57361f8
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-10 12:58:11 -0600

    remove current routes

--- server/server.js
+++ server/server.js
@@ -27,48 +27,6 @@ app.use(
 );
 
 /* Routes */
-app.use("/api/posts/admin", admin);
-
-app.use("/api/posts/posts", async (req, res) => {
-  const allPosts = await db.collection("posts").find().toArray();
-  res.json(allPosts);
-});
-
-app.delete("/api/posts/:id", async (req, res) => {
-  if (typeof req.params.id != "string") req.params.id = "";
-  db.collection("posts").deleteOne({ _id: new ObjectId(req.params.id) });
-  res.send("good job");
-});
-
-app.post("/api/posts/create-post", async (req, res) => {
-  const info = await db.collection("posts").insertOne(req.cleanData);
-  const newPost = await db
-    .collection("posts")
-    .findOne({ _id: new ObjectId(info.insertedId) });
-  res.send(newPost);
-});
-
-app.put("/api/posts/update-post/:id", async (req, res) => {
-  db.collection("posts").findOneAndUpdate(
-    { _id: new ObjectId(req.params.id) },
-    {
-      $set: {
-        title: req.body.draftTitle,
-        topic: req.body.draftTopic,
-        description: req.body.draftDescription,
-      },
-    }
-  );
-
-  res.send(false);
-});
-
-app.use("/api/posts", async (req, res) => {
-  const allPosts = await db.collection("posts").find().toArray();
-  res.render("home", { allPosts });
-});
-
-app.use("*", main);
 
 // Port
 app.listen(port, () => {
