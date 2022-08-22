291a8fc10ff180a69bb02f74dff5ad69e83b777e
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-22 10:52:23 -0600

    create Post class

---
+++ server/classes/Post.js
@@ -0,0 +1,11 @@
+class Post {
+  constructor(title, description, externalLink) {
+    this.title = title;
+    this.description = description;
+    this.externalLink = externalLink;
+  }
+}
+
+module.exports = {
+  Post,
+};
