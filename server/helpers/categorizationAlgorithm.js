36f5f42568f2a8ff314136f6f211eb101ee0dc3d
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-23 13:58:40 -0600

    leverage Category object to store categories

--- server/helpers/categorizationAlgorithm.js
+++ server/helpers/categorizationAlgorithm.js
@@ -1,28 +1,34 @@
+const { Category } = require("../classes/category");
+
 // Categorize reddit post into relevant categories based on common post attributes
 const categorizeData = (posts) => {
   // Sorting categories
-  var discussions = [];
-  var news = [];
-  var general = [];
+  var discussionsArray = [];
+  var newsArray = [];
+  var generalArray = [];
 
   for (var i = 0; i < posts.length; i++) {
     // A post fits into the Discussions category if it has an associated user discussion (a description)
     if (posts[i].description != null) {
-      discussions.push(posts[i]);
+      discussionsArray.push(posts[i]);
     }
 
-    // A posts[i] fits into the News category if it lacks a description but includes a non-YouTube link
+    // A post fits into the News category if it lacks a description but includes a non-YouTube link
     else if (
       posts[i].externalLink != null &&
       !posts[i].externalLink.includes("https://www.youtube.com")
     ) {
-      news.push(posts[i]);
+      newsArray.push(posts[i]);
     }
-    // A posts[i] fits into the General category lacks a description and includes a non-YouTube link or no link
+    // A post fits into the General category lacks a description and includes a non-YouTube link or no link
     else {
-      general.push(posts[i]);
+      generalArray.push(posts[i]);
     }
   }
+
+  var discussions = new Category("discussions", discussionsArray);
+  var news = new Category("news", newsArray);
+  var general = new Category("general", generalArray);
 
   var categorizedData = [discussions, news, general];
 
