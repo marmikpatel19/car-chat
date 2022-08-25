ec6929abc6b35e28ea7f4916606d602dee26a059
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-25 15:20:07 -0600

    integrate handling for POST (create entry) request

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,6 +1,7 @@
 const axios = require("axios").default;
 const { categorizeData } = require("../helpers/categorizationAlgorithm");
 const { createPosts } = require("../helpers/postManagement");
+const postsModel = require("../models/postsModel");
 
 // Posts from Reddit API
 var posts;
@@ -48,7 +49,33 @@ const getCategories = async (req, res) => {
   res.send([discussions, news, general]);
 };
 
+// Create a set of posts
+const createSet = async (req, res) => {
+  const savePosts = new postsModel({
+    date: new Date().toISOString().slice(0, 10),
+    categories: {
+      categoryOne: {
+        name: discussions.name,
+        posts: discussions.posts,
+      },
+      categoryTwo: {
+        name: news.name,
+        posts: news.posts,
+      },
+      categoryThree: {
+        name: general.name,
+        posts: general.posts,
+      },
+    },
+  });
+
+  savePosts
+    .save()
+    .catch((err) => console.log("Error adding document to mongodb: " + err));
+};
+
 module.exports = {
   getPosts,
   getCategories,
+  createSet,
 };
