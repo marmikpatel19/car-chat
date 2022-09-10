ed89eb4cb6229131de26dc9f32ed33474e01551e
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-09-10 14:03:26 -0600

    establish routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import "./styling/app.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const Home = import("./components/pages/home");
 
 function App() {
-  
   return (
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
