922d679c41e52a2eb5abbfc16a4c804c506ff96f
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-09-11 12:14:14 -0600

    add and style page title

---
+++ client/src/components/pages/home.jsx
@@ -0,0 +1,11 @@
+import "../../styling/home.css";
+
+function Home() {
+  return (
+    <div id="home">
+      <h1 id="home-title">Explore what’s trending in the automobile world!</h1>
+    </div>
+  );
+}
+
+export default Home;
