715b0004c802ff790b41bbbb6bba22a7a51b5af8
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-09-13 21:17:38 -0600

    create and style primary button

--- client/src/components/pages/home.jsx
+++ client/src/components/pages/home.jsx
@@ -1,9 +1,25 @@
 import "../../styling/home.css";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 function Home() {
   return (
     <div id="home">
       <h1 id="home-title">Explore what’s trending in the automobile world!</h1>
+      <Typography component="div" align="center">
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{
+            fontWeight: "900",
+            fontSize: "1.3rem",
+            ["@media screen and (min-width: 550px)"]: { fontSize: "1.5rem" },
+          }}
+          id="home-button"
+        >
+          See Posts
+        </Button>
+      </Typography>
     </div>
   );
 }
