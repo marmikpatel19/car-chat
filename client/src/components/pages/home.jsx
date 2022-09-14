fdea002305b7e4373020b10f7f88651c380bc360
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-09-13 21:35:34 -0600

    integrate conditional rendering for onClick()

--- client/src/components/pages/home.jsx
+++ client/src/components/pages/home.jsx
@@ -1,8 +1,21 @@
 import "../../styling/home.css";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { useState } from "react";
 
 function Home() {
+  // Button click to fetch data
+  const [isclicked, setIsClicked] = useState(false);
+
+  // Conditional rendering of the categories component
+  let categories;
+
+  if (isclicked) {
+    categories = <p>temp categories</p>;
+  } else {
+    categories = <></>;
+  }
+
   return (
     <div id="home">
       <h1 id="home-title">Explore what’s trending in the automobile world!</h1>
@@ -16,10 +29,12 @@ function Home() {
             ["@media screen and (min-width: 550px)"]: { fontSize: "1.5rem" },
           }}
           id="home-button"
+          onClick={() => setIsClicked(true)}
         >
           See Posts
         </Button>
       </Typography>
+      {categories}
     </div>
   );
 }
