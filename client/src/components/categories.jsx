c31df721be08710bb58e1dfff37af790acb76e74
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-09-30 12:01:06 -0600

    integrate conditionally rendering button states

--- client/src/components/categories.jsx
+++ client/src/components/categories.jsx
@@ -1,47 +1,69 @@
 import "../styling/categories.css";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { useState } from "react";
 
 function Categories() {
+  // Hooks for buttons
+  const [isNewsClicked, setIsNewsClicked] = useState(true);
+  const [isDiscussionsClicked, setIsDiscussionsClicked] = useState(false);
+  const [isGeneralClicked, setIsGeneralClicked] = useState(false);
+
   return (
     <div id="categories">
       <Typography component="div" align="center">
         <Button
           variant="contained"
-          color="primary"
+          {...(isNewsClicked ? { color: "secondary" } : { color: "primary" })}
           sx={{
             fontWeight: "900",
             fontSize: "1.3rem",
             ["@media screen and (min-width: 550px)"]: { fontSize: "1.5rem" },
           }}
           id="categories-buttons"
-          /*onClick={() => setIsClicked(true)}*/
+          onClick={() => {
+            setIsNewsClicked(true);
+            setIsDiscussionsClicked(false);
+            setIsGeneralClicked(false);
+          }}
         >
           News
         </Button>
         <Button
           variant="contained"
-          color="primary"
+          {...(isDiscussionsClicked
+            ? { color: "secondary" }
+            : { color: "primary" })}
           sx={{
             fontWeight: "900",
             fontSize: "1.3rem",
             ["@media screen and (min-width: 550px)"]: { fontSize: "1.5rem" },
           }}
           id="categories-buttons"
-          /*onClick={() => setIsClicked(true)}*/
+          onClick={() => {
+            setIsNewsClicked(false);
+            setIsDiscussionsClicked(true);
+            setIsGeneralClicked(false);
+          }}
         >
           Discussions
         </Button>
         <Button
           variant="contained"
-          color="primary"
+          {...(isGeneralClicked
+            ? { color: "secondary" }
+            : { color: "primary" })}
           sx={{
             fontWeight: "900",
             fontSize: "1.3rem",
             ["@media screen and (min-width: 550px)"]: { fontSize: "1.5rem" },
           }}
           id="categories-buttons"
-          /* onClick={() => setIsClicked(true)}*/
+          onClick={() => {
+            setIsNewsClicked(false);
+            setIsDiscussionsClicked(false);
+            setIsGeneralClicked(true);
+          }}
         >
           general
         </Button>
